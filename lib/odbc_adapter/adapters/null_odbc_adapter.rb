5bf451c346b56602b38040e9c0b227257ec51d18
Montana Low <montanalow@users.noreply.github.com>
2020-04-10 12:43:54 -0700

    Update null_odbc_adapter.rb

--- lib/odbc_adapter/adapters/null_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/null_odbc_adapter.rb
@@ -4,6 +4,9 @@ module ODBCAdapter
     # registry. This allows for minimal support for DBMSs for which we don't
     # have an explicit adapter.
     class NullODBCAdapter < ActiveRecord::ConnectionAdapters::ODBCAdapter
+      class BindSubstitution < Arel::Visitors::ToSql	
+      end
+
       # Using a BindVisitor so that the SQL string gets substituted before it is
       # sent to the DBMS (to attempt to get as much coverage as possible for
       # DBMSs we don't support).
